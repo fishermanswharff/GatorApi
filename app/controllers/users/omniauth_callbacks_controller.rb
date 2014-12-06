b4c1fd7f47db615d84822e024f796adc8a8eb444
fishermanswharff <fishermanswharff@github.com>
2014-12-06 16:34:10 -0500

    Autopush Gator2 API OmniAuth Branch

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,2 +1,6 @@
 class Users::OmniauthCallbacksController < ApplicationController
+
+  def passthru
+    # provider = request.filtered_parameters['provider']
+  end
 end
--- spec/requests/omniauth_callbacks_spec.rb
+++ spec/requests/omniauth_callbacks_spec.rb
@@ -2,6 +2,26 @@ require 'rails_helper'
 require 'spec_helper'
 
 describe 'Omniauth Callbacks Controller' do
-  it 'routes to the appropriate provider' do
+
+  describe '#passthru' do
+    let(:provider) do
+      'twitter'
+    end
+    before(:each) do
+      post "/users/auth/#{provider}",{}
+    end
+
+    it 'responds successfully' do
+      # I expect this get request to send a request to twitters request token endpoint, and return
+      # a token: https://api.twitter.com/oauth/request_token
+      expect(response.status).to eq 200
+    end
+
+    it 'routes to the appropriate provider' do
+      expect(request.filtered_parameters['provider']).to eq 'twitter'
+    end
+
+    
   end
+  
 end
