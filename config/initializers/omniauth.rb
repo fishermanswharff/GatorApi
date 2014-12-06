896b89fddec037bdf6937be1fa220db4ada4feb4
fishermanswharff <fishermanswharff@github.com>
2014-12-06 13:12:55 -0500

    Autopush Gator2 API OmniAuth Branch

--- config/initializers/omniauth.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :developer unless Rails.env.production?
-  provider :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_CONSUMER_SECRET']
-end
