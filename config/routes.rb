31f33bca28e885b0e7a5ef1d60d402c3c5865c85
fishermanswharff <fishermanswharff@github.com>
2014-12-05 21:46:50 -0500

    auto pushed Gator2 updates

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :users, except: [:new, :edit]
   post '/login', to: 'users#login'
   get '/logout', to: 'users#logout'
+  get '/' => redirect('https://github.com/fishermanswharff/GatorApi')
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
