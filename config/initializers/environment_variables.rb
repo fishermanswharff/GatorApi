3242c78c054a7a0220215a46a268d0522c4cc4c7
fishermanswharff <fishermanswharff@github.com>
2014-12-29 16:19:00 -0700

    environment variables

--- config/initializers/environment_variables.rb
+++ config/initializers/environment_variables.rb
@@ -1,7 +1,7 @@
-module TwitterVariables
+module EnvironmentVariables
   class Application < Rails::Application
     config.before_configuration do
-      env_file = Rails.root.join("config", 'twitter.yml').to_s
+      env_file = Rails.root.join("config", 'env.yml').to_s
       if File.exists?(env_file)
         YAML.load_file(env_file)[Rails.env].each do |key, value|
           ENV[key.to_s] = value
