dbb82213da529903dce9a414ae96e6709f8b39f1
fishermanswharff <fishermanswharff@github.com>
2014-12-29 16:46:39 -0700

    Autopush Gator2 API Master Branch

--- config/initializers/environment_variables.rb
+++ config/initializers/environment_variables.rb
@@ -4,6 +4,7 @@ module EnvironmentVariables
       env_file = Rails.root.join("config", 'env.yml').to_s
       if File.exists?(env_file)
         YAML.load_file(env_file)[Rails.env].each do |key, value|
+          binding.pry
           ENV[key.to_s] = value
         end
       end
