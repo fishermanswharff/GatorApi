54ba6efaff47c3f38f1dd617e4e35623cde909c2
fishermanswharff <fishermanswharff@github.com>
2015-01-01 16:07:42 -0500

    Autopush Gator2 API Master Branch

--- lib/modules/OAuth.rb
+++ lib/modules/OAuth.rb
@@ -33,10 +33,28 @@ module OAuth
     def version
       "1.0"
     end
+
+    def escape(value)
+      CGI::escape(value)
+    end
   end
 end
 
 =begin
 https://dev.twitter.com/oauth/overview/creating-signatures
+Every oauth_* parameter needs to be included in the signature
+
+POST
+https://api.twitter.com/oauth/request_token
+oauth_callback=http://127.0.0.1/users/auth/twitter/callback
+oauth_consumer_key=ENV['TWITTER_CONSUMER_KEY']
+oauth_nonce=SecureRandom.uuid.gsub(/[\-\n\r]/,'')
+oauth_signature_method=HMAC-SHA1
+oauth_timestamp=Time.now.utc.strftime('%s').to_i
+oauth_token=ENV['TWITTER_ACCESS_TOKEN']
+oauth_version=1.0
+
+to create the signature, 
 
 =end
+
