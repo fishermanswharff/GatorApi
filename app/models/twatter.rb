b4ba4b02d1db99f23862de400874804d1ae99cf4
fishermanswharff <fishermanswharff@github.com>
2015-10-12 17:35:12 -0500

    refactoring twatter model

--- app/models/twatter.rb
+++ app/models/twatter.rb
@@ -1,19 +1,13 @@
 class Twatter
 
-  attr_accessor :current_user, :auth, :client
+  attr_accessor :client
 
   def initialize(user)
-    @current_user = user
-    @auth = @current_user.user_authentications.map { |i| i if i.authentication_provider.name == 'twitter' }.first
-    token = @auth.params["oauth_token"]
-    secret = @auth.params["oauth_token_secret"]
-    screen_name = @auth.params["screen_name"]
-    @client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
-      config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
-      config.access_token        = token
-      config.access_token_secret = secret
-    end
+    auth = get_auth(user)
+    token = auth.params["oauth_token"]
+    secret = auth.params["oauth_token_secret"]
+    screen_name = auth.params["screen_name"]
+    @client = get_client(token,secret)
   end
 
   def get_tweets
@@ -21,4 +15,18 @@ class Twatter
       self.client.home_timeline
     end
   end
+
+  def get_auth(user)
+    user.user_authentications.map { |i| i if i.authentication_provider.name == 'twitter' }.first
+  end
+
+  def get_client(token,secret)
+    Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+      config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+      config.access_token        = token
+      config.access_token_secret = secret
+    end
+  end
+
 end
